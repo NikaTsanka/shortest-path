11778ee19e5d0940ed50c586d5de6f45ad8e5964
Nika Tsankashvili <nikatsanka@outlook.com>
2016-10-16 13:21:32 -0400

    working-progress

--- main.cpp
+++ main.cpp
@@ -8,8 +8,295 @@
 #include <X11/Xos.h>
 #include <math.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 using namespace std;
+
+/*******************************************************************************/
+
+// A structure to represent a node in adjacency list
+struct AdjListNode
+{
+    int dest;
+    int weight;
+    struct AdjListNode* next;
+};
+
+// A structure to represent an adjacency liat
+struct AdjList
+{
+    struct AdjListNode *head;  // pointer to head node of list
+};
+
+// A structure to represent a graph. A graph is an array of adjacency lists.
+// Size of array will be V (number of vertices in graph)
+struct Graph
+{
+    int V;
+    struct AdjList* array;
+};
+
+// A utility function to create a new adjacency list node
+struct AdjListNode* newAdjListNode(int dest, int weight)
+{
+    struct AdjListNode* newNode =
+            (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// A utility function that creates a graph of V vertices
+struct Graph* createGraph(int V)
+{
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->V = V;
+
+    // Create an array of adjacency lists.  Size of array will be V
+    graph->array = (struct AdjList*) malloc(V * sizeof(struct AdjList));
+
+    // Initialize each adjacency list as empty by making head as NULL
+    for (int i = 0; i < V; ++i)
+        graph->array[i].head = NULL;
+
+    return graph;
+}
+
+// Adds an edge to an undirected graph
+void addEdge(struct Graph* graph, int src, int dest, int weight)
+{
+    // Add an edge from src to dest.  A new node is added to the adjacency
+    // list of src.  The node is added at the begining
+    struct AdjListNode* newNode = newAdjListNode(dest, weight);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+
+    // Since graph is undirected, add an edge from dest to src also
+    newNode = newAdjListNode(src, weight);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+
+// Structure to represent a min heap node
+struct MinHeapNode
+{
+    int  v;
+    int dist;
+};
+
+// Structure to represent a min heap
+struct MinHeap
+{
+    int size;      // Number of heap nodes present currently
+    int capacity;  // Capacity of min heap
+    int *pos;     // This is needed for decreaseKey()
+    struct MinHeapNode **array;
+};
+
+// A utility function to create a new Min Heap Node
+struct MinHeapNode* newMinHeapNode(int v, int dist)
+{
+    struct MinHeapNode* minHeapNode =
+            (struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+    minHeapNode->v = v;
+    minHeapNode->dist = dist;
+    return minHeapNode;
+}
+
+// A utility function to create a Min Heap
+struct MinHeap* createMinHeap(int capacity)
+{
+    struct MinHeap* minHeap =
+            (struct MinHeap*) malloc(sizeof(struct MinHeap));
+    minHeap->pos = (int *)malloc(capacity * sizeof(int));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array =
+            (struct MinHeapNode**) malloc(capacity * sizeof(struct MinHeapNode*));
+    return minHeap;
+}
+
+// A utility function to swap two nodes of min heap. Needed for min heapify
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b)
+{
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// A standard function to heapify at given idx
+// This function also updates position of nodes when they are swapped.
+// Position is needed for decreaseKey()
+void minHeapify(struct MinHeap* minHeap, int idx)
+{
+    int smallest, left, right;
+    smallest = idx;
+    left = 2 * idx + 1;
+    right = 2 * idx + 2;
+
+    if (left < minHeap->size &&
+        minHeap->array[left]->dist < minHeap->array[smallest]->dist )
+        smallest = left;
+
+    if (right < minHeap->size &&
+        minHeap->array[right]->dist < minHeap->array[smallest]->dist )
+        smallest = right;
+
+    if (smallest != idx)
+    {
+        // The nodes to be swapped in min heap
+        MinHeapNode *smallestNode = minHeap->array[smallest];
+        MinHeapNode *idxNode = minHeap->array[idx];
+
+        // Swap positions
+        minHeap->pos[smallestNode->v] = idx;
+        minHeap->pos[idxNode->v] = smallest;
+
+        // Swap nodes
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+
+        minHeapify(minHeap, smallest);
+    }
+}
+
+// A utility function to check if the given minHeap is ampty or not
+int isEmpty(struct MinHeap* minHeap)
+{
+    return minHeap->size == 0;
+}
+
+// Standard function to extract minimum node from heap
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+    if (isEmpty(minHeap))
+        return NULL;
+
+    // Store the root node
+    struct MinHeapNode* root = minHeap->array[0];
+
+    // Replace root node with last node
+    struct MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+    minHeap->array[0] = lastNode;
+
+    // Update position of last node
+    minHeap->pos[root->v] = minHeap->size-1;
+    minHeap->pos[lastNode->v] = 0;
+
+    // Reduce heap size and heapify root
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+
+    return root;
+}
+
+// Function to decreasy dist value of a given vertex v. This function
+// uses pos[] of min heap to get the current index of node in min heap
+void decreaseKey(struct MinHeap* minHeap, int v, int dist)
+{
+    // Get the index of v in  heap array
+    int i = minHeap->pos[v];
+
+    // Get the node and update its dist value
+    minHeap->array[i]->dist = dist;
+
+    // Travel up while the complete tree is not hepified.
+    // This is a O(Logn) loop
+    while (i && minHeap->array[i]->dist < minHeap->array[(i - 1) / 2]->dist)
+    {
+        // Swap this node with its parent
+        minHeap->pos[minHeap->array[i]->v] = (i-1)/2;
+        minHeap->pos[minHeap->array[(i-1)/2]->v] = i;
+        swapMinHeapNode(&minHeap->array[i],  &minHeap->array[(i - 1) / 2]);
+
+        // move to parent index
+        i = (i - 1) / 2;
+    }
+}
+
+// A utility function to check if a given vertex
+// 'v' is in min heap or not
+bool isInMinHeap(struct MinHeap *minHeap, int v)
+{
+    if (minHeap->pos[v] < minHeap->size)
+        return true;
+    return false;
+}
+
+// A utility function used to print the solution
+void printArr(int dist[], int n)
+{
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < n; ++i)
+        printf("%d \t\t %d\n", i, dist[i]);
+}
+
+// The main function that calulates distances of shortest paths from src to all
+// vertices. It is a O(ELogV) function
+void dijkstra(struct Graph* graph, int src)
+{
+    int V = graph->V;// Get the number of vertices in graph
+    int dist[V];      // dist values used to pick minimum weight edge in cut
+
+    // minHeap represents set E
+    struct MinHeap* minHeap = createMinHeap(V);
+
+    // Initialize min heap with all vertices. dist value of all vertices
+    for (int v = 0; v < V; ++v)
+    {
+        dist[v] = INT_MAX;
+        minHeap->array[v] = newMinHeapNode(v, dist[v]);
+        minHeap->pos[v] = v;
+    }
+
+    // Make dist value of src vertex as 0 so that it is extracted first
+    minHeap->array[src] = newMinHeapNode(src, dist[src]);
+    minHeap->pos[src]   = src;
+    dist[src] = 0;
+    decreaseKey(minHeap, src, dist[src]);
+
+    // Initially size of min heap is equal to V
+    minHeap->size = V;
+
+    // In the followin loop, min heap contains all nodes
+    // whose shortest distance is not yet finalized.
+    while (!isEmpty(minHeap))
+    {
+        // Extract the vertex with minimum distance value
+        struct MinHeapNode* minHeapNode = extractMin(minHeap);
+        int u = minHeapNode->v; // Store the extracted vertex number
+
+        // Traverse through all adjacent vertices of u (the extracted
+        // vertex) and update their distance values
+        struct AdjListNode* pCrawl = graph->array[u].head;
+        while (pCrawl != NULL)
+        {
+            int v = pCrawl->dest;
+
+            // If shortest distance to v is not finalized yet, and distance to v
+            // through u is less than its previously calculated distance
+            if (isInMinHeap(minHeap, v) && dist[u] != INT_MAX &&
+                pCrawl->weight + dist[u] < dist[v])
+            {
+                dist[v] = dist[u] + pCrawl->weight;
+
+                // update distance value in min heap also
+                decreaseKey(minHeap, v, dist[v]);
+            }
+            pCrawl = pCrawl->next;
+        }
+    }
+
+    // print the calculated shortest distances
+    printArr(dist, V);
+}
+
+/*******************************************************************************/
+
+
+
 
 bool ends_with(const std::string &, const std::string &);
 
@@ -19,9 +306,9 @@ string parse_string(std::string);
 
 int det(int, int, int, int, int, int);
 
-void pair_generator(int, int, const int [][2]);
+void pair_generator(int, int, const int **, const Graph *graph);
 
-void check_edge(vector<int>,const int [][2]);
+void check_edge(vector<int>, const int **, const Graph *graph);
 
 vector<string> split(const string &s, char delim) {
     vector<string> elems;
@@ -44,6 +331,8 @@ GC gc;
 void init_x();
 void close_x();
 void redraw();
+
+int calc_dist(const int x1, const int y1, const int x2, const int y2);
 
 int main(int argc, char *argv[]) {
     // cout << "Hello, World!" << endl;
@@ -190,13 +479,13 @@ int main(int argc, char *argv[]) {
                         int rows = sizeof(coordinates) / sizeof(coordinates[0]);
                         int cols = sizeof(coordinates[0]) / sizeof(int);
 
-                        int array[rows * 3][2];
+                        int shrinked_array[rows * 3][2];
                         int k = 0;
 
                         for (int i = 0; i < rows * 3;) {
                             for (int j = 0; j < cols; j += 2) {
-                                array[i][0] = coordinates[k][j];
-                                array[i][1] = coordinates[k][j + 1];
+                                shrinked_array[i][0] = coordinates[k][j];
+                                shrinked_array[i][1] = coordinates[k][j + 1];
                                 i++;
                             }
                             k++;
@@ -204,12 +493,20 @@ int main(int argc, char *argv[]) {
 
                         /*for (int i = 0; i < rows * 3; i++) {
                              for (int j = 0; j < 2; j++) {
-                                    printf("array[%d][%d] = %d\n", i,j, array[i][j] );
+                                    printf("shrinked_array[%d][%d] = %d\n", i,j, shrinked_array[i][j] );
                              }
                              printf("\n");
                          }*/
 
-                        pair_generator(0, 4, array);
+                        int vert = (rows * 3 ) * 2;
+
+                        // create the graph
+
+                        struct Graph* graph = createGraph(vert);
+
+                        pair_generator(0, 4, shrinked_array, graph);
+
+                        dijkstra(graph, 0);
 
 
                     } else {
@@ -337,65 +634,83 @@ int main(int argc, char *argv[]) {
 
 
 
-void pair_generator(int offset, int k, const int coord[][2]) {
+void pair_generator(int offset, int k, const int **coord, const Graph *graph) {
     if (k == 0) {
-        check_edge(pairs, coord);
+        check_edge(pairs, coord, graph);
         return;
     }
     for (int i = offset; i <= points.size() - k; i++) {
         pairs.push_back(points[i]);
         // recurse
-        pair_generator(i + 1, k - 1, coord);
+        pair_generator(i + 1, k - 1, coord, graph);
         pairs.pop_back();
     }
 }
 
-void check_edge(vector<int> combination, const int array[][2]) {
+void check_edge(vector<int> combination, const int **coord_array, const Graph *graph) {
     static int count = 0;
     cout << "combination no " << (count++) << ": [ ";
     for (int i = 0; i < combination.size(); i++) {
-        cout << combination[i] << " (" << array[combination[i]][0] << "," << array[combination[i]][1] << ") ";
+        cout << combination[i] << " (" << coord_array[combination[i]][0] << "," << coord_array[combination[i]][1] << ") ";
     }
     cout << "] " << endl;
 
     /*Just the formula
-    If (((det (p, q , r) * det(p q s) ) < 0)
-    And
+        If (((det (p, q , r) * det(p q s) ) < 0)
+            And
      ((Det (s r p) * det ( s r q))< 0)
-Then the edge pq intersects sr*/
+
+        Then the edge pq intersects sr*/
 
     int pqr = 0, pqs = 0, srp = 0, srq = 0;
+
+    int distpq = 0, distsr = 0;
 
     // p - 0, q - 1, r - 2, s - 3
     // x - 0, y - y
 
 
-    pqr = det(array[combination[0]][0],array[combination[0]][1], // p
-              array[combination[1]][0],array[combination[1]][1], // q
-              array[combination[2]][0],array[combination[2]][1]); // r
+    pqr = det(coord_array[combination[0]][0],coord_array[combination[0]][1], // p
+              coord_array[combination[1]][0],coord_array[combination[1]][1], // q
+              coord_array[combination[2]][0],coord_array[combination[2]][1]); // r
 
-    pqs = det(array[combination[0]][0],array[combination[0]][1],
-              array[combination[1]][0],array[combination[1]][1],
-              array[combination[3]][0],array[combination[3]][1]);
+    pqs = det(coord_array[combination[0]][0],coord_array[combination[0]][1],
+              coord_array[combination[1]][0],coord_array[combination[1]][1],
+              coord_array[combination[3]][0],coord_array[combination[3]][1]);
 
-    srp = det(array[combination[3]][0],array[combination[3]][1],
-              array[combination[2]][0],array[combination[2]][1],
-              array[combination[0]][0],array[combination[0]][1]);
+    srp = det(coord_array[combination[3]][0],coord_array[combination[3]][1],
+              coord_array[combination[2]][0],coord_array[combination[2]][1],
+              coord_array[combination[0]][0],coord_array[combination[0]][1]);
 
-    srq = det(array[combination[3]][0],array[combination[3]][1],
-              array[combination[2]][0],array[combination[2]][1],
-              array[combination[1]][0],array[combination[1]][1]);
+    srq = det(coord_array[combination[3]][0],coord_array[combination[3]][1],
+              coord_array[combination[2]][0],coord_array[combination[2]][1],
+              coord_array[combination[1]][0],coord_array[combination[1]][1]);
 
     if (((pqr * pqs) < 0) && ((srp * srq) < 0)) {
         cout << "Intersection\n";
     } else {
         cout << "Nope\n";
+        // calc distance from p to q, r to s
+        distpq = calc_dist(coord_array[combination[0]][0],coord_array[combination[0]][1], // p
+                           coord_array[combination[1]][0],coord_array[combination[1]][1]);
+
+        distsr = calc_dist(coord_array[combination[2]][0],coord_array[combination[2]][1], // p
+                           coord_array[combination[3]][0],coord_array[combination[3]][1]);
+
+        // add to the graph by index
+
+        addEdge((Graph *) graph, combination[0], distpq, combination[1] );
+
+        addEdge((Graph *) graph, combination[2], distsr, combination[3] );
+
     }
 
 
 }
 
-
+int calc_dist(const int x1, const int y1, const int x2, const int y2) {
+    return (int) sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
 
 
 int det(int px, int py, int qx, int qy, int rx, int ry) {
